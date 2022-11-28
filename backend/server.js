e6ce0ffe7d923b13768bdb45b9ab4b0a142ad5b6
pasinduwickrama <pasinduwickramasinghe@gmail.com>
2022-11-28 21:56:37 +0530

    host

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const app = express();
 // middler
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["http://localhost:3000" , "http://mern-task-app.onrender.com"],
+    }
+));
 
 app.use("/api/tasks", taskRoutes);
 
